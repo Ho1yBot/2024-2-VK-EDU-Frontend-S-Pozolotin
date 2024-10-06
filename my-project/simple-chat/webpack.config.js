77f77e8f42126d3a2550d0aec398bdd500d1267e
Svyatoslav <pozolotin-cvat@yandex.ru>
2024-10-06 20:07:36 +0300

    тест путей к картинками

--- my-project/simple-chat/webpack.config.js
+++ my-project/simple-chat/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
     output: {
         path: BUILD_PATH,
         filename: 'bundle.js',
-        publicPath: './',
+        publicPath: '/',
     },
     module: {
         strictExportPresence: true,
