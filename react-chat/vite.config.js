c9168184cd854321edc323c3c3cc40b745b961cf
Svyatoslav <pozolotin-cvat@yandex.ru>
2024-12-11 21:16:14 +0300

    Fix base path for GitHub Pages

--- react-chat/vite.config.js
+++ react-chat/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/2024-2-VK-EDU-Frontend-S-Pozolotin/', // Укажите путь репозитория
   server: {
     proxy: {
       '/api': {
