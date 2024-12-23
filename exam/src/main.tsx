0cb3b5aa33d0d3c25a0d5ec87ea535c4022ed25c
Svyatoslav <pozolotin-cvat@yandex.ru>
2024-12-24 00:00:50 +0300

    фикс белого экрана gh pages

--- exam/src/main.tsx
+++ exam/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import store from './store';
 import App from './App';
 import './index.css';
@@ -8,7 +9,9 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router basename="/2024-2-VK-EDU-Frontend-S-Pozolotin/">
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>
 );
